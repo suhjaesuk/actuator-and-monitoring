c5e06fa1fc6343ac3ffc735e481710ba9bd372b8
suhjaesuk <suhjaesuk@gmail.com>
2023-04-24 11:57:12 +0900

    actuator 기본 설정법

--- src/main/java/hello/actuator/ActuatorApplication.java
+++ src/main/java/hello/actuator/ActuatorApplication.java
@@ -1,7 +1,9 @@
 package hello.actuator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorApplication {
@@ -10,4 +12,8 @@ public class ActuatorApplication {
         SpringApplication.run(ActuatorApplication.class, args);
     }
 
+    @Bean
+    public InMemoryHttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
